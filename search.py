69140214a6471239bdfa5dcad41c4edd962c9dc6
Sukumar Gaonkar <sukumargaonkar@gmail.com>
2018-11-10 10:40:26 -0500

    Visualization Complete

--- search.py
+++ search.py
@@ -1,6 +1,8 @@
 import random
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 import numpy as np
-
 
 class GameMaster:
 
@@ -68,6 +70,52 @@ class DummyPlayer:
         self.dimen = len(map)
         self.belief = self.create_belief_matrix(self.dimen)
 
+        self.marker = 5
+        map[0][0] += self.marker
+        self.prev_cell = (0, 0)
+
+        plt.ion()
+        self.fig = plt.figure(figsize=(10, 6))
+        self.ax = plt.subplot(1, 2, 1)
+        self.belief_plot = self.ax.matshow(self.belief, cmap='Greys')
+        plt.colorbar(self.belief_plot)
+
+        cdict1 = {'red': ((0.0, 1.0, 1.0),
+                          (0.2, 0.8, 0.8),
+                          (0.4, 0.6, 0.6),
+                          (0.6, 0.4, 0.4),
+                          (0.8, 0.2, 0.2),
+                          (1, 0, 0)),
+
+                  'green': ((0.0, 1.0, 1.0),
+                            (0.2, 0.8, 0.8),
+                            (0.4, 0.6, 0.6),
+                            (0.6, 0.4, 0.4),
+                            (0.8, 0.2, 0.2),
+                            (1, 0, 0)),
+
+                  'blue': ((0.0, 1.0, 1.0),
+                           (0.2, 0.8, 0.8),
+                           (0.4, 0.6, 0.6),
+                           (0.6, 0.4, 0.4),
+                           (0.8, 0.2, 0.2),
+                           (1, 0, 0))
+                  }
+
+        # terrain_colors = [[x/255.0 for x in l] for l in [[102, 51, 0], [0, 153, 0], [166, 166, 166], [255, 51, 0]]]
+        # terrain_colors = [[x/255.0 for x in l] for l in [[199, 233, 192], [161, 217, 155], [116, 196, 118], [49, 163, 84], [0,0,0]]]
+        # terrain_colors = plt.cm.Greens
+        # terrain_colors = ['gray', 'green', 'brown', 'black', 'blue']
+        # map[3][3] += 5
+        cm = LinearSegmentedColormap('BlueRed1', cdict1)
+        # cm = matplotlib.colors.ListedColormap(terrain_colors)
+        self.terrain_plot = self.fig.add_subplot(1, 2, 2).matshow(map, cmap=cm)
+        plt.show()
+
+
+    def update_belief_mat_animation(self,data):
+        self.belief_plot.set_data(self.belief)
+
     def create_belief_matrix(self, dimen):
         belief = [[1 / (dimen * dimen) for j in range(dimen)] for i in range(dimen)]
         return belief
@@ -76,16 +124,22 @@ class DummyPlayer:
         print(np.matrix(self.belief))
 
     def update_belief_matrix(self, user_cell):
-        if self.map[user_cell[0]][user_cell[1]] == 0:
+
+        if self.map[user_cell[0]][user_cell[1]] > 3:
+            cell_val = self.map[user_cell[0]][user_cell[1]] - self.marker
+        else:
+            cell_val = self.map[user_cell[0]][user_cell[1]]
+
+        if cell_val == 0:     # Plains
             fnr = 0.1
-        elif self.map[user_cell[0]][user_cell[1]] == 1:
+        elif cell_val == 1:   # Hilly
             fnr = 0.3
-        elif self.map[user_cell[0]][user_cell[1]] == 2:
+        elif cell_val == 2:   # Forest
             fnr = 0.7
-        elif self.map[user_cell[0]][user_cell[1]] == 3:
+        elif cell_val == 3:   # Caves
             fnr = 0.9
 
-        sum = 1 - self.belief[user_cell[0]][user_cell[1]]
+        sum = 1 - (1 - fnr) * self.belief[user_cell[0]][user_cell[1]]
         scaling_fac = (1 - fnr) * self.belief[user_cell[0]][user_cell[1]] / sum
         self.belief[user_cell[0]][user_cell[1]] *= fnr
 
@@ -93,8 +147,6 @@ class DummyPlayer:
             for j in range(self.dimen):
                 if (i, j) != user_cell:
                     self.belief[i][j] *= (1 + scaling_fac)
-        # self.printMap(10, self.belief)
-        # self.dummy_player(self.dimen, self.map, user_cell, self.belief)
 
     def next_move(self):
 
@@ -105,6 +157,11 @@ class DummyPlayer:
                 if self.belief[i][j] == y:
                     user_cell = (i, j)
 
+        if self.prev_cell is not None:
+            self.map[self.prev_cell[0]][self.prev_cell[1]] -= self.marker
+
+        self.prev_cell = user_cell
+        self.map[user_cell[0]][user_cell[1]] += self.marker
         return user_cell
 
 
@@ -116,6 +173,9 @@ if __name__ == "__main__":
 
     dummy_player.printBelief()
 
+    print("Dimen: {}  Target: {},{}".format(dimen, game_master.target[0], game_master.target[1]))
+
+    input("Press 'Y' Key to continue")      # just to stop the execution before entering while loop
     i = 0
 
     while not game_won:
@@ -123,7 +183,13 @@ if __name__ == "__main__":
         user_cell = dummy_player.next_move()
         game_won = game_master.search_cell(user_cell)
         dummy_player.update_belief_matrix(user_cell)
-        print("\nMove - {}: {},{} \n".format(i,user_cell[0],user_cell[1]))
+        print("\nMove - {}: {},{}\n".format(i,user_cell[0],user_cell[1]))
         dummy_player.printBelief()
+        dummy_player.terrain_plot.set_data(dummy_player.map)
+        dummy_player.belief_plot.set_data(dummy_player.belief)
+        dummy_player.fig.canvas.draw()
+        plt.pause(0.001)
+        # time.sleep(2)
 
-    print("Kudos you Won!!!")
+    print("Kudos you Won!!!\n{} cells Searched".format(i))
+    input()
