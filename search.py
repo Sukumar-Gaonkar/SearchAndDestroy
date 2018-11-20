8153d43e944d2d501a3a3a49bdb8e845ad186917
Sukumar Gaonkar <sukumargaonkar@gmail.com>
2018-11-20 16:04:54 -0500

    Added switch to turn off visualization

--- search.py
+++ search.py
@@ -22,10 +22,11 @@ def get_fnr(cell_val):
 
 class GameMaster:
 
-    def __init__(self, dimen, moving_target):
+    def __init__(self, dimen, moving_target, visualize):
         self.target_hit_counter = 0
         self.target_marker = 20
         self.moving_target = moving_target
+        self.visualize = visualize
 
         self.game_map = self.genMap(dimen)
         self.target = self.create_random_target()
@@ -45,41 +46,43 @@ class GameMaster:
                     gamemap[i][j] = 2  # forested terrain betwwen 0.5 and 0.8
                 elif num > 0.8 and num <= 1:
                     gamemap[i][j] = 3  # caves terrain graeter than 0.8
+        # return [[0, 2, 3], [2, 2, 1], [1, 2, 3]]
         return gamemap
 
     def create_random_target(self):
-        while True:
-            x = np.random.randint(0, len(self.game_map))
-            y = np.random.randint(0, len(self.game_map))
-            if self.game_map[x][y] == 3:
-                break
+        # while True:
+        x = np.random.randint(0, len(self.game_map))
+        y = np.random.randint(0, len(self.game_map))
+            # if self.game_map[x][y] == 3:
+                # break
         return (x, y)
 
     def search_cell(self, user_target):
 
         if tuple(self.target) == user_target:
-            # if self.target_hit_counter == 5:
-            #     self.target_hit_counter = 0
-            #     return True
-            # else:
-            #     self.target_hit_counter += 1
-            #     return False
+            reqd_hits = [2, 2, 4, 10]
+            if self.target_hit_counter == reqd_hits[self.game_map[user_target[0]][user_target[1]] % 5]:
+                self.target_hit_counter = 0
+                return True
+            else:
+                self.target_hit_counter += 1
+                return False
 
-            num1 = random.random()
-            terrain_type = self.game_map[self.target[0]][self.target[1]]
-            terrain_type = terrain_type - 5 if terrain_type >= 5 else terrain_type
-            if terrain_type == 0:
-                if num1 > 0.1:
-                    return True
-            elif terrain_type == 1:
-                if num1 > 0.3:
-                    return True
-            elif terrain_type == 2:
-                if num1 > 0.7:
-                    return True
-            elif terrain_type == 3:
-                if num1 > 0.9:
-                    return True
+            # num1 = random.random()
+            # terrain_type = self.game_map[self.target[0]][self.target[1]]
+            # terrain_type = terrain_type - 5 if terrain_type >= 5 else terrain_type
+            # if terrain_type == 0:
+            #     if num1 > 0.1:
+            #         return True
+            # elif terrain_type == 1:
+            #     if num1 > 0.3:
+            #         return True
+            # elif terrain_type == 2:
+            #     if num1 > 0.7:
+            #         return True
+            # elif terrain_type == 3:
+            #     if num1 > 0.9:
+            #         return True
 
         # You got False
 
@@ -88,8 +91,9 @@ class GameMaster:
         else:
             crossing = [0, 0]
 
-            self.dummy_player.hit_freq[self.target[0]][self.target[1]] -= self.target_marker
-            self.dummy_player.hit_freq_texts[self.target[0]][self.target[1]].set_text(str(self.dummy_player.hit_freq[self.target[0]][self.target[1]]))
+            if visualize:
+                self.dummy_player.hit_freq[self.target[0]][self.target[1]] -= self.target_marker
+                self.dummy_player.hit_freq_texts[self.target[0]][self.target[1]].set_text(str(self.dummy_player.hit_freq[self.target[0]][self.target[1]]))
 
             crossing[0] = self.game_map[self.target[0]][self.target[1]]
 
@@ -103,9 +107,10 @@ class GameMaster:
 
             self.target = temp_target
             crossing[1] = self.game_map[self.target[0]][self.target[1]]
-            self.dummy_player.hit_freq[temp_target[0]][temp_target[1]] += self.target_marker
-            self.dummy_player.hit_freq_texts[temp_target[0]][temp_target[1]].set_text(
-                str(self.dummy_player.hit_freq[temp_target[0]][temp_target[1]]))
+            if visualize:
+                self.dummy_player.hit_freq[temp_target[0]][temp_target[1]] += self.target_marker
+                self.dummy_player.hit_freq_texts[temp_target[0]][temp_target[1]].set_text(
+                    str(self.dummy_player.hit_freq[temp_target[0]][temp_target[1]]))
 
             return crossing
 
@@ -120,17 +125,19 @@ class GameMaster:
 
     def set_player(self, player):
         self.dummy_player = player
-        self.dummy_player.hit_freq[self.target[0]][self.target[1]] += self.target_marker
+        if visualize:
+            self.dummy_player.hit_freq[self.target[0]][self.target[1]] += self.target_marker
 
 
 class DummyPlayer:
 
-    def __init__(self, game_map, moving_target, case, rule, iteration):
+    def __init__(self, game_map, moving_target, case, rule, iteration, visualize):
         self.map = game_map
         self.dimen = len(game_map)
         self.rule = rule
         self.case = case
-        self.hit_freq = [[0 for j in range(self.dimen)] for i in range(self.dimen)]
+        if visualize:
+            self.hit_freq = [[0 for j in range(self.dimen)] for i in range(self.dimen)]
         self.belief = self.create_belief_matrix(self.dimen)
         self.moving_target = moving_target
         self.confidence_mat = self.create_belief_matrix(self.dimen)
@@ -154,51 +161,52 @@ class DummyPlayer:
         game_map[0][0] += self.marker
         self.prev_cell = (0, 0)
 
-        plt.ion()
-        self.fig = plt.figure(figsize=(10, 6))
-        self.fig.suptitle("Iteration: {}  Case: {}  Rule: {}".format(iteration, case_str[case], rule_str[rule]))
-        # self.ax = plt.subplot(1, 3, 1)
-        self.belief_plot = plt.subplot(2, 2, 1).matshow(self.belief, cmap='Greys')
-        # self.belief_plot.set_clim(0, 1)
-        plt.colorbar(self.belief_plot)
+        if visualize:
+            plt.ion()
+            self.fig = plt.figure(figsize=(10, 6))
+            self.fig.suptitle("Iteration: {}  Case: {}  Rule: {}".format(iteration, case_str[case], rule_str[rule]))
+            # self.ax = plt.subplot(1, 3, 1)
+            self.belief_plot = plt.subplot(2, 2, 1).matshow(self.belief, cmap='Greys')
+            # self.belief_plot.set_clim(0, 1)
+            plt.colorbar(self.belief_plot)
 
-        self.confidence_plot = plt.subplot(2, 2, 2).matshow(self.confidence_mat, cmap='Greys')
-        # self.confidence_plot.set_clim(0, 1)
-        plt.colorbar(self.confidence_plot)
+            self.confidence_plot = plt.subplot(2, 2, 2).matshow(self.confidence_mat, cmap='Greys')
+            # self.confidence_plot.set_clim(0, 1)
+            plt.colorbar(self.confidence_plot)
 
-        cdict1 = {'red': ((0.0, 1.0, 1.0),
-                          (0.2, 0.8, 0.8),
-                          (0.4, 0.6, 0.6),
-                          (0.6, 0.4, 0.4),
-                          (0.8, 0.2, 0.2),
-                          (1, 0, 0)),
-                  'green': ((0.0, 1.0, 1.0),
-                            (0.2, 0.8, 0.8),
-                            (0.4, 0.6, 0.6),
-                            (0.6, 0.4, 0.4),
-                            (0.8, 0.2, 0.2),
-                            (1, 0, 0)),
-                  'blue': ((0.0, 1.0, 1.0),
-                           (0.2, 0.8, 0.8),
-                           (0.4, 0.6, 0.6),
-                           (0.6, 0.4, 0.4),
-                           (0.8, 0.2, 0.2),
-                           (1, 0, 0))
-                  }
+            cdict1 = {'red': ((0.0, 1.0, 1.0),
+                              (0.2, 0.8, 0.8),
+                              (0.4, 0.6, 0.6),
+                              (0.6, 0.4, 0.4),
+                              (0.8, 0.2, 0.2),
+                              (1, 0, 0)),
+                      'green': ((0.0, 1.0, 1.0),
+                                (0.2, 0.8, 0.8),
+                                (0.4, 0.6, 0.6),
+                                (0.6, 0.4, 0.4),
+                                (0.8, 0.2, 0.2),
+                                (1, 0, 0)),
+                      'blue': ((0.0, 1.0, 1.0),
+                               (0.2, 0.8, 0.8),
+                               (0.4, 0.6, 0.6),
+                               (0.6, 0.4, 0.4),
+                               (0.8, 0.2, 0.2),
+                               (1, 0, 0))
+                      }
 
-        cm = LinearSegmentedColormap('BlueRed1', cdict1)
-        # cm = matplotlib.colors.ListedColormap(terrain_colors)
-        self.terrain_plot = self.fig.add_subplot(2, 2, 3).matshow(game_map, cmap=cm)
+            cm = LinearSegmentedColormap('BlueRed1', cdict1)
+            # cm = matplotlib.colors.ListedColormap(terrain_colors)
+            self.terrain_plot = self.fig.add_subplot(2, 2, 3).matshow(game_map, cmap=cm)
 
-        self.freq_ax = self.fig.add_subplot(2, 2, 4)
-        self.freq_plot = self.freq_ax.matshow(self.hit_freq, cmap="Oranges")
+            self.freq_ax = self.fig.add_subplot(2, 2, 4)
+            self.freq_plot = self.freq_ax.matshow(self.hit_freq, cmap="Oranges")
 
-        self.hit_freq_texts = [[None for i in range(self.dimen)] for i in range(self.dimen)]
+            self.hit_freq_texts = [[None for i in range(self.dimen)] for i in range(self.dimen)]
 
-        for (i, j), z in np.ndenumerate(self.hit_freq):
-            self.hit_freq_texts[i][j] = self.freq_ax.text(j, i, '{}'.format(z), ha='center', va='center')
+            for (i, j), z in np.ndenumerate(self.hit_freq):
+                self.hit_freq_texts[i][j] = self.freq_ax.text(j, i, '{}'.format(z), ha='center', va='center')
 
-        plt.show()
+            plt.show()
 
     def isvalid(self, target):
         if target[0] > -1 and target[0] < len(self.map) and target[1] > -1 and target[1] < len(self.map):
@@ -302,15 +310,15 @@ class DummyPlayer:
                 for i in range(self.dimen):
                     for j in range(self.dimen):
                         #   print(self.k)
-                        if self.map[i][j] == active_terrain:
+                        if self.map[i][j] % self.marker == active_terrain:
                             if self.rule == 1:
                                 # Belief Matrix
-                                if self.belief[i][j] > maxm:
+                                if self.belief[i][j] >= maxm:
                                     maxm = self.belief[i][j]
                                     user_cell = (i, j)
                             elif self.rule == 2:
                                 # Confidence Matrix
-                                if self.confidence_mat[i][j] > maxm:
+                                if self.confidence_mat[i][j] >= maxm:
                                     maxm = self.confidence_mat[i][j]
                                     user_cell = (i, j)
 
@@ -337,10 +345,11 @@ class DummyPlayer:
             self.prev_cell = user_cell
             self.map[user_cell[0]][user_cell[1]] += self.marker
 
-            self.hit_freq[user_cell[0]][user_cell[1]] += 1
-            hit_count = self.hit_freq[user_cell[0]][user_cell[1]]
-            # self.freq_ax.text(user_cell[0], user_cell[1], '{}'.format(hit_count), ha='center', va='center')
-            self.hit_freq_texts[user_cell[0]][user_cell[1]].set_text(str(hit_count))
+            if visualize:
+                self.hit_freq[user_cell[0]][user_cell[1]] += 1
+                hit_count = self.hit_freq[user_cell[0]][user_cell[1]]
+                # self.freq_ax.text(user_cell[0], user_cell[1], '{}'.format(hit_count), ha='center', va='center')
+                self.hit_freq_texts[user_cell[0]][user_cell[1]].set_text(str(hit_count))
 
             move_counter += 1
             yield user_cell
@@ -350,22 +359,22 @@ if __name__ == "__main__":
     # dimen = 15
 
     moving_target = False
+    visualize = False
 
-    iterations = 1
+    iterations = 10
     fileName = "results/profilerResults_" + time.strftime("%Y%m%d-%H%M%S") + ".csv"
     f = open(fileName, "a", 1)
-    f.write("Gridsize;IterationNo;Case;Rule;Searches\n")
+    f.write("Gridsize;IterationNo;Terrain;Case;Rule;Searches\n")
 
-    for itr in range(1, iterations+1):
-        # Iteration Loop
-        for dimen in range(5, 11):
-
-            game_master = GameMaster(dimen, moving_target)
+    for dimen in range(3, 30):
+        for itr in range(1, iterations+1):
+            # Iteration Loop
+            game_master = GameMaster(dimen, moving_target, visualize)
 
             for case in range(1, 3):
                 for rule in range(1, 3):
 
-                    dummy_player = DummyPlayer(game_master.game_map, moving_target, case, rule, iterations)
+                    dummy_player = DummyPlayer(game_master.game_map, moving_target, case, rule, iterations, visualize)
                     game_master.set_player(dummy_player)
                     move_generator = dummy_player.next_move()
                     game_won = False
@@ -392,21 +401,26 @@ if __name__ == "__main__":
 
                         dummy_player.border_cross_detected(user_cell, crossing)
                         # dummy_player.update_belief_matrix(user_cell)
-                        print("\nMove - {}: {},{}\n".format(i, user_cell[0], user_cell[1]))
+                        # print("\nMove - {}: {},{}\n".format(i, user_cell[0], user_cell[1]))
 
                         # dummy_player.printBelief()
-                        dummy_player.terrain_plot.set_data(dummy_player.map)
-                        dummy_player.belief_plot.set_data(dummy_player.belief)
-                        dummy_player.confidence_plot.set_data(dummy_player.confidence_mat)
-                        dummy_player.freq_plot.set_data(dummy_player.hit_freq)
-                        dummy_player.freq_plot.set_clim(0, game_master.target_marker)
-                        dummy_player.fig.canvas.draw()
-                        plt.pause(0.001)
+                        if visualize:
+                            dummy_player.terrain_plot.set_data(dummy_player.map)
+                            dummy_player.belief_plot.set_data(dummy_player.belief)
+                            dummy_player.confidence_plot.set_data(dummy_player.confidence_mat)
+                            dummy_player.freq_plot.set_data(dummy_player.hit_freq)
+                            dummy_player.freq_plot.set_clim(0, game_master.target_marker)
+                            dummy_player.fig.canvas.draw()
+                            plt.pause(0.001)
                         # time.sleep(0.9)
 
                     game_master.game_map[game_master.target[0]][game_master.target[1]] -= dummy_player.marker
-                    plt.close(dummy_player.fig)
+                    if visualize:
+                        plt.close(dummy_player.fig)
 
+                    target = game_master.target
 
+                    target_terrain = terrains[game_master.game_map[target[0]][target[1]]]
                     print("Kudos you Won!!!\n{} cells Searched".format(i))
+                    f.write("{0};{1};{2};{3};{4};{5}\n".format(dimen, itr, target_terrain, case_str[case], rule_str[rule], i))
                     # input()
